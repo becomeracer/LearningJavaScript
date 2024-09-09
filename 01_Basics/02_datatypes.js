af990ce76e83c1dc0ffe13ab5372fd90ab269537
becomeracer <becomeracer@gmail.com>
2024-09-09 18:18:30 +0000

    datatype and typeof function.

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all js code as newer version.
+//alert (3+3) // we are using nodejs, not browser.
+//console.log(3+3);
+//console.log("hello");
+
+ 
+let name = "jamal"
+let age = 50
+let isLoggedIn = true
+let state;
+let sym = null;
+let temperature = "hello" ;
+
+// Number range: 2 to pwoer 53
+//BigInt is used to store large number values, may be use in trading or stocks companies.
+// string => ""
+//Boolean => true/false
+//null  => standalone value or object
+//undefined => defined as underfined
+//Symbol => unique
+//Object => null is also known as object
+
+
+
+console.log("temperature is " + typeof  temperature)
+console.log("age is " + typeof age, "isLoggedIn is " + typeof isLoggedIn);
+console.log("state is " + typeof state, "null is " + typeof null);
+console.log("symbol is " + typeof sym, "name is " + typeof name)
+ 
+
+ 
