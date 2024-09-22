6652bd867235add712076d38d8f17daf869672f0
becomeracer <becomeracer@gmail.com>
2024-09-22 16:17:02 +0000

    performing various conversions

---
+++ 01_Basics/03_conversionOperationsExt.js
@@ -0,0 +1,65 @@
+let value = 3
+let negValue = -value
+//console.log(negValue); // => -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(12%5); // => 2 remainder
+// console.log(2**4); // 2 raise to the power 4
+
+let str1 = "hello"
+let str2 =" world"
+let str3 = str1 + str2
+//console.log(str3); // => hello world
+
+// console.log("1" + 2); // => 12
+// console.log(2 + "1"); // => 21
+// console.log("1" + 2 + 2); // => 122
+// console.log(1 + "2" + "2"); // => 122
+// console.log(1 + 2 + "2"); // => 32
+
+// console.log(true); // => true
+// console.log(+true); // => 1
+// console.log(-true); // => -1
+// console.log(++true); // ERROR: invalid left-hand side expression in prefix operation.
+// console.log(--true); // ERROR: invalid left-hand side expression in prefix operation.
+// console.log(true++); // ERROR: invalid left-hand side expression in prefix operation.
+// console.log(true--); // ERROR: invalid left-hand side expression in prefix operation.
+// console.log(-""); // => 0
+// console.log(-""); // => -0
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter); // => 101
+// ++gameCounter
+// console.log(gameCounter); // => 102
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
